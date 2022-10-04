4d71f6885327b51332e5b994b3634c9d5f21ea12
3amrabdelwahab <3amr.abdelwahab@gmail.com>
2022-10-04 07:06:04 +0200

    adding Landing Page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Select Landing Page
+let landingPage = document.querySelector('.landingPage');
+
+// Get Array from Img
+let imgArray = [
+    "BG-01.png",
+    "BG-02.png",
+    "BG-03.png",
+    "BG-04.png",
+    "BG-05.png"
+];
+
+setInterval ( () => {
+    // Get Random Number
+    let randomNumber = Math.floor(Math.random() * imgArray.length);
+
+    // Change background img
+    landingPage.style.backgroundImage = 'url("img/'+ imgArray[randomNumber] +'")';
+
+}, 5000 );
+
+// Spin Gear 
+let gearCont = document.querySelector('.gearCont');
+let settingsBox = document.querySelector('.settingsBox');
+let gear = document.querySelector('.gear');
+
+gearCont.addEventListener('click', ()=> {
+    settingsBox.classList.toggle('open');
+    gear.classList.toggle('open');
+});
+
+// function openSettings () {
+//     gearCont.classList.toggle('active');
+//     gear.classList.toggle('active');
+// }
